3362776616d8ec5264743e66453f4ec5fa7ae18e
Diego <diego.jimenez@noseryoung.ch>
2026-06-26 10:39:40 +0200

    add game

--- src/main/java/dj/tamagotchi/HelloController.java
+++ src/main/java/dj/tamagotchi/HelloController.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.image.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
 public class HelloController {
@@ -14,36 +17,36 @@ public class HelloController {
     private Pet pet = new Pet();
     @FXML
     public ImageView petImage;
-
     @FXML
-    private Button playButton;
+    public Button playButton;
+    @FXML
+    public Button eatButton;
+    @FXML
+    public Button sleepButton;
+    @FXML
+    public Label infoText;
+    @FXML
+    public AnchorPane mainPane;
+    @FXML
+    public AnchorPane playPane;
+    @FXML
+    public ImageView chooseImage1;
+    @FXML
+    public ImageView chooseImage2;
+    @FXML
+    public ImageView chooseImage3;
 
     @FXML
     public void initialize() {
-        perrys.put(States.HAPPY, new Image(getClass().getResourceAsStream("images/happy.png")));
-        perrys.put(States.HUNGRY, new Image(getClass().getResourceAsStream("images/hungry.png")));
-        perrys.put(States.SAD, new Image(getClass().getResourceAsStream("images/sad.png")));
-        perrys.put(States.SLEEPING, new Image(getClass().getResourceAsStream("images/sleeping.png")));
-        perrys.put(States.TIRED, new Image(getClass().getResourceAsStream("images/tired.png")));
-        perrys.put(States.HUNGRY_TIRED, new Image(getClass().getResourceAsStream("images/hungry_tired.png")));
-        perrys.put(States.DEAD, new Image(getClass().getResourceAsStream("images/dead.jpg")));
+        playPane.setVisible(false);
+        playPane.setManaged(false);
+        mainPane.setVisible(true);
+        mainPane.setManaged(true);
+        loadImages();
         petImage.setImage(perrys.get(States.HAPPY));
-
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), e -> updatePet()));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
-    }
-
-    public void onPlayButtonClick() {
-        pet.play();
-    }
-
-    public void onSleepButtonClick() {
-        pet.sleep();
-    }
-
-    public void onEatButtonClick() {
-        pet.eat();
     }
 
     private void updatePet() {
@@ -56,8 +59,41 @@ public class HelloController {
         System.out.println("Happiness: " + pet.getHappiness());
         System.out.println("---------------------------------");
     }
-}
 
-/*
- * How to dissable button button.setDisable(false)
- */
+    private void loadImages() {
+        perrys.put(States.HAPPY, new Image(getClass().getResourceAsStream("images/happy.png")));
+        perrys.put(States.HUNGRY, new Image(getClass().getResourceAsStream("images/hungry.png")));
+        perrys.put(States.SAD, new Image(getClass().getResourceAsStream("images/sad.png")));
+        perrys.put(States.SLEEPING, new Image(getClass().getResourceAsStream("images/sleeping.png")));
+        perrys.put(States.TIRED, new Image(getClass().getResourceAsStream("images/tired.png")));
+        perrys.put(States.HUNGRY_TIRED, new Image(getClass().getResourceAsStream("images/hungry_tired.png")));
+        perrys.put(States.DEAD, new Image(getClass().getResourceAsStream("images/dead.jpg")));
+        perrys.put(States.EATING, new Image(getClass().getResourceAsStream("images/eating.png")));
+        perrys.put(States.FAKE, new Image(getClass().getResourceAsStream("images/fake_perry.png")));
+        perrys.put(States.OTHER_FAKE, new Image(getClass().getResourceAsStream("images/fake_perry_2.png")));
+        perrys.put(States.CUBE, new Image(getClass().getResourceAsStream("images/cube.png")));
+    }
+
+    public void onPlayButtonClick() {
+        pet.play(this);
+    }
+
+    public void onSleepButtonClick() {
+        petImage.setImage(perrys.get(States.SLEEPING));
+        pet.sleep((message) -> showMessage(message), playButton, eatButton, sleepButton);
+    }
+
+    public void onEatButtonClick() {
+        petImage.setImage(perrys.get(States.EATING));
+        pet.eat(petImage, () -> showMessage("Perry has eaten"), playButton, eatButton, sleepButton);
+    }
+
+    private void showMessage(String message) {
+        infoText.setText(message);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3), event -> {
+            infoText.setText("");
+        }));
+        timeline.setCycleCount(1);
+        timeline.play();
+    }
+}
--- src/main/java/dj/tamagotchi/Pet.java
+++ src/main/java/dj/tamagotchi/Pet.java
@@ -1,6 +1,12 @@
 package dj.tamagotchi;
 
+import java.util.function.Consumer;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class Pet {
     private int energy;
@@ -15,38 +21,15 @@ public class Pet {
         this.happiness = MAX_VALUE;
     }
 
-    public void eat() {
-        appetite = Math.min(appetite + 10, MAX_VALUE);
-    }
-
-    public void play() {
-        happiness = Math.min(happiness + 10, MAX_VALUE);
-    }
-
-    public void sleep() {
-        energy = Math.min(energy + 10, MAX_VALUE);
-    }
-
-    public void decreaseEnergie(int amount) {
-        energy = Math.max(energy - amount, 0);
-    }
-
-    public void decreaseHappines(int amount) {
-        happiness = Math.max(happiness - amount, 0);
-    }
-
-    public void decreaseAppetite(int amount) {
-        appetite = Math.max(appetite - amount, 0);
-    }
-
     public void updateStatus(ImageView img) {
+        // TODO: find a way to add status effects like sleepy, overfeed etc
         boolean isHungry = appetite <= CRITICAL_VALUE ? true : false;
         boolean isSad = happiness <= CRITICAL_VALUE ? true : false;
         boolean isTired = energy <= CRITICAL_VALUE ? true : false;
         if (isHungry && isSad && isTired) {
             img.setImage(HelloController.perrys.get(States.DEAD));
         } else if (isHungry && isSad) {
-            // TODO: add images
+            // TODO: add images that are matching
         } else if (isHungry && isTired) {
             img.setImage(HelloController.perrys.get(States.HUNGRY_TIRED));
         } else if (isSad && isTired) {
@@ -60,7 +43,141 @@ public class Pet {
         } else {
             img.setImage(HelloController.perrys.get(States.HAPPY));
         }
+    }
 
+    /**
+     * 
+     * @param img image to change
+     * @param onFinishedAction run this action once the timeline is done
+     * @param buttons buttons to dissable
+     */
+    public void eat(ImageView img, Runnable onFinishedAction, Button... buttons) {
+        int amount = (int) (Math.random() * 8) + 3;
+        for (Button button : buttons) {
+            button.setDisable(true);
+        }
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(amount), event -> {
+            for (Button button : buttons) {
+                button.setDisable(false);
+            }
+            appetite = Math.min(appetite + amount, MAX_VALUE);
+            energy = Math.min(energy + (int) (amount / 2), MAX_VALUE);
+
+            updateStatus(img);
+
+            if (onFinishedAction != null) {
+                onFinishedAction.run();
+            }
+        });
+
+        Timeline timeline = new Timeline(keyFrame);
+        timeline.setCycleCount(1);
+        timeline.play();
+    }
+
+    public void play(HelloController controller) {
+        controller.mainPane.setVisible(false);
+        controller.mainPane.setManaged(false);
+        controller.playPane.setVisible(true);
+        controller.playPane.setManaged(true);
+
+        controller.infoText.setText("Select the right Perry");
+        int spot = (int) (Math.random() * 3) + 1;
+        int switchUp = (int) (Math.random() * 2) + 1;
+        States fake1 = (switchUp == 1) ? States.FAKE : States.OTHER_FAKE;
+        States fake2 = (switchUp == 1) ? States.OTHER_FAKE : States.FAKE;
+
+        switch (spot) {
+        case 1:
+            controller.chooseImage1.setImage(HelloController.perrys.get(States.CUBE));
+            controller.chooseImage2.setImage(HelloController.perrys.get(fake1));
+            controller.chooseImage3.setImage(HelloController.perrys.get(fake2));
+
+            controller.chooseImage1.setOnMouseClicked(e -> handlePlayResult(true, controller));
+            controller.chooseImage2.setOnMouseClicked(e -> handlePlayResult(false, controller));
+            controller.chooseImage3.setOnMouseClicked(e -> handlePlayResult(false, controller));
+            break;
+
+        case 2:
+            controller.chooseImage1.setImage(HelloController.perrys.get(fake1));
+            controller.chooseImage2.setImage(HelloController.perrys.get(States.CUBE));
+            controller.chooseImage3.setImage(HelloController.perrys.get(fake2));
+
+            controller.chooseImage1.setOnMouseClicked(e -> handlePlayResult(false, controller));
+            controller.chooseImage2.setOnMouseClicked(e -> handlePlayResult(true, controller));
+            controller.chooseImage3.setOnMouseClicked(e -> handlePlayResult(false, controller));
+            break;
+
+        case 3:
+            controller.chooseImage1.setImage(HelloController.perrys.get(fake1));
+            controller.chooseImage2.setImage(HelloController.perrys.get(fake2));
+            controller.chooseImage3.setImage(HelloController.perrys.get(States.CUBE));
+
+            controller.chooseImage1.setOnMouseClicked(e -> handlePlayResult(false, controller));
+            controller.chooseImage2.setOnMouseClicked(e -> handlePlayResult(false, controller));
+            controller.chooseImage3.setOnMouseClicked(e -> handlePlayResult(true, controller));
+            break;
+        }
+    }
+
+    private void handlePlayResult(boolean won, HelloController controller) {
+        controller.chooseImage1.setOnMouseClicked(null);
+        controller.chooseImage2.setOnMouseClicked(null);
+        controller.chooseImage3.setOnMouseClicked(null);
+
+        if (won) {
+            happiness = Math.min(happiness + 15, MAX_VALUE);
+        } else {
+            happiness = Math.min(happiness + 2, MAX_VALUE);
+        }
+
+        decreaseEnergie(5);
+
+        controller.playPane.setVisible(false);
+        controller.playPane.setManaged(false);
+        controller.mainPane.setVisible(true);
+        controller.mainPane.setManaged(true);
+    }
+
+    public void sleep(Consumer<String> onFinishedAction, Button... buttons) {
+        int amount = (int) (Math.random() * 10) + 1;
+        for (Button button : buttons) {
+            button.setDisable(true);
+        }
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(amount), event -> {
+            for (Button button : buttons) {
+                button.setDisable(false);
+            }
+            energy = Math.min(energy + amount * 5, MAX_VALUE);
+            String message = "";
+            if (amount < 4) {
+                message = "Perry has not slept well";
+            } else if (amount < 7) {
+                message = "Perry has slept okay";
+            } else {
+                message = "Perry has slept wonderfull";
+            }
+
+            if (onFinishedAction != null) {
+                onFinishedAction.accept(message);
+            }
+        });
+        Timeline timeline = new Timeline(keyFrame);
+        timeline.setCycleCount(1);
+        timeline.play();
+
+    }
+
+    public void decreaseEnergie(int amount) {
+        energy = Math.max(energy - amount, 0);
+    }
+
+    public void decreaseHappines(int amount) {
+        happiness = Math.max(happiness - amount, 0);
+    }
+
+    public void decreaseAppetite(int amount) {
+        appetite = Math.max(appetite - amount, 0);
     }
 
     public int getEnergy() {
